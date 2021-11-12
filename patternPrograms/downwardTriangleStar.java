11c7c949a6b6c1481ddb642851102a7393609835
k.a.ricks-pennington <k.a.ricks-pennington@accenturefederal.com>
2021-11-12 10:52:06 -0600

    downwardTriangleStarPattern

---
+++ patternPrograms/downwardTriangleStar.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args) {
+        printStarPattern(5);
+    }
+
+    public static void printStarPattern(int rows) {
+        int i, j;
+        for (i=0; i<rows; i++) {
+            for(j=i; j<rows; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
